38166f9fc82978d54e184ccbfd2233cd9a96e378
mercklyzer <mercklyzer@gmail.com>
2024-09-07 10:33:33 +0800

    feat: add create post

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -6,5 +6,22 @@ module Api
       posts, posts_meta = paginate(Post.all)
       payload(data: posts, meta: posts_meta, status: 200)
     end
+
+    def create
+      post = Post.new(post_params)
+      saved_ok = post.save
+
+      if saved_ok
+        payload(data: post, status: 200)
+      else
+        payload(errors: post.errors.map(&:full_message), status: 400)
+      end
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  skip_forgery_protection
+
   def payload(data: [], meta: [], errors: [], status: 200)
     render json: { data: data, meta: meta, errors: errors }, status: status
   end
